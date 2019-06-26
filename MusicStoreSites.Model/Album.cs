1db88d5e67ed412d78ef823221fe00c628a5ba13
SemaNurYanik <nursemayanik@gmail.com>
2019-06-26 13:56:31 +0300

    Core+Model

---
+++ MusicStoreSites.Model/Album.cs
@@ -0,0 +1,23 @@
+﻿using MusicStoreSites.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreSites.Model
+{
+    public class Album :BaseEntity
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public short Stock { get; set; }
+        public string AlbumArtUrl { get; set; }
+        public bool Discounted { get; set; }
+
+        public int ArtistID { get; set; }
+
+
+        public virtual Artist Artist { get; set; }
+    }
+}
