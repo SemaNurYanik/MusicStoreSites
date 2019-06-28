490acd2e69124a49a450881f0878d2041d19c0cc
SemaNurYanik <nursemayanik@gmail.com>
2019-06-28 15:01:10 +0300

    280619_SON

--- MusicStoreSites.DAL/Concrete/EntityFramework/MusicStoreDbContext.cs
+++ MusicStoreSites.DAL/Concrete/EntityFramework/MusicStoreDbContext.cs
@@ -12,7 +12,8 @@ namespace MusicStoreSites.DAL.Concrete.EntityFramework
     {
         public MusicStoreDbContext():base("server=.;database=MusicStoreSitesDB;integrated security=true;")
         {
-
+            Database.SetInitializer<MusicStoreDbContext>(new MyStrategy());
+            //dbContext ayağa kalkarken bir strateji kullanmak istedik, bu strateji ile birlikte dbContext oluştuğunda yani veritabanı sql tarafında oluştuğunda içinde hazır veriler de olsun, bunun için DropCreateDatabaseIfModelChanges yaklaşımını kullandık.
         }
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
@@ -23,7 +24,14 @@ namespace MusicStoreSites.DAL.Concrete.EntityFramework
         public DbSet<User> UserDetails { get; set; }
 
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Properties().Where(a => a.PropertyType == typeof(DateTime)).Configure(a => a.HasColumnType("datetime2"));
 
+            modelBuilder.Configurations.Add(new OrderDetailMapping());
+
+            //modellerimin içindeki propertylerden tip olarak datetime kullanan varsa, sql tarafında karşılığını bulamaz diye tipini çevirme işlemi yapıyoruz.modelle ilgili değişiklik yaptığımız içinde bunu OnModelCreating metodunu override ederek yazıyoruz.
+        }
 
 
 
---
+++ MusicStoreSites.UI.MVC/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using MusicStoreSites.BLL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStoreSites.UI.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+
+        IUserService userService;
+        public AccountController(IUserService UserService)
+        {
+            userService = UserService;
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+
+        //NINJECT İLE DEVAM EDİLECEK!!!!!!!!!!
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
