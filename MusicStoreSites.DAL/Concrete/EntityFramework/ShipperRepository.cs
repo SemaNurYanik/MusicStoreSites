f94a408eb6404db45546c79e972586e7876ea724
SemaNurYanik <nursemayanik@gmail.com>
2019-06-28 09:28:19 +0300

    Guncel2

--- MusicStoreSites.DAL/Concrete/EntityFramework/ShipperRepository.cs
+++ MusicStoreSites.DAL/Concrete/EntityFramework/ShipperRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MusicStoreSites.Core.DAL.EntityFramework;
+using MusicStoreSites.DAL.Abstract;
+using MusicStoreSites.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MusicStoreSites.DAL.Concrete.EntityFramework
 {
-    class ShipperRepository
+    public class ShipperRepository:EFRepositoryBase<Shipper,MusicStoreDbContext>,IShipperDAL
     {
     }
 }
