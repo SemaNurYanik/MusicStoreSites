cc94bcf85f0672bc2d0ba2b4ca991b6e64b33e9b
SemaNurYanik <nursemayanik@gmail.com>
2019-07-01 15:05:35 +0300

    010719_3

--- MusicStoreSites.UI.MVC/Controllers/AccountController.cs
+++ MusicStoreSites.UI.MVC/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using MusicStoreSites.BLL.Abstract;
+using MusicStoreSites.Model;
+using MusicStoreSites.UI.MVC.Tools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,9 +25,46 @@ namespace MusicStoreSites.UI.MVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            try
+            {
+                userService.Insert(user);
+                bool sonuc = MailHelper.SendConfirmationMail(user.UserName, user.Password, user.Email, user.ID);
+                if (!sonuc)
+                {
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Veritabanı ekleme hatası!";
+                return View();
+            }
+            return RedirectToAction("Login", "Account");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var gelenKullanici = userService.GetUserByLogin(user.UserName, user.Password);
+            if (gelenKullanici != null)
+            {
+                Session["kullanici"] = gelenKullanici;
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = "Kullanıcı bulunamadı";
+            return View();
+        }
 
 
-     
         public ActionResult Index()
         {
             return View();
